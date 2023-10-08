f19374e053a7e0a26b79189f59f042cb2847d31b
shubham369963 <shubhamhalade369@gmail.com>
2023-10-08 21:26:02 +0530

    day 1

---
+++ basic.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+// #include<bits/stdc++.h>
+
+
+/*
+multi line
+*/
+
+//single line
+
+using namespace std;
+
+int main(){
+    
+    //day - 1
+    
+    //output
+    cout<<"hello world"<<"\n";
+    cout<<"hello world"<<endl;
+
+    int a,b;
+
+    cin>>a>>b;
+
+    cout<<a<<" "<<b;
+
+    //memory type
+    //stack - static memory - compile time 
+    //heap - dynamic memory - run time 
+
+    //1 bit = 0 or 1 - 2^1
+    //1byte = 8bit - 2^8-1 - 255
+    //int - 4byte - 2^32-1 
+    //long - 8byte
+
+    //char - 1byte
+    //bool - 1bit
+    //float - 4byte
+    //double - 8byte
+
+    
+
+
+
+    return 0;
+}
