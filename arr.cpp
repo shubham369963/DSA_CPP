4823510326a21ad78afee06a0a27e00d49473537
shubham369963 <shubhamhalade369@gmail.com>
2023-10-09 23:46:31 +0530

    linear search, rev array

---
+++ arr.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // int arr[4];
+
+    // for(int i=0; i<4; i++){
+    //     cin>>arr[i];
+    // }
+
+    int left = 0;
+    int right = 3;
+
+    while(left < right){
+        swap(arr[left], arr[right]);
+
+        // int temp = arr[left];
+        // arr[left] = arr[right];
+        // arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    // for(int i=0; i<4; i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    int arr[4];
+
+    for(int i=0; i<4; i++){
+        cin>>arr[i];
+    }
+    int key = 5;
+
+    for(int i=0; i<4; i++){
+        if(arr[i] == key){
+            cout<<"present"<<endl;
+        }
+    }
+
+    return 0;
+}
